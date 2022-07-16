590b7c12a93000f9512610081ad358e0d5671ad8
pintuproject <104198297+pintuproject@users.noreply.github.com>
2022-07-16 20:08:59 +0530

    Add files via upload

---
+++ Superclass.java
@@ -0,0 +1,20 @@
+public class Superclass {
+
+    public void printMethod() {
+        System.out.println("Printed in Superclass.");
+    }
+}
+ 
+ class Subclass extends Superclass {
+
+    // overrides printMethod in Superclass
+    public void printMethod() {
+        super.printMethod();
+        System.out.println("Printed in Subclass");
+    }
+    public static void main(String[] args) {
+        Subclass s = new Subclass();
+        s.printMethod();    
+    }
+}
+ 
